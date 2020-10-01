254afcc247d941ab7de80dc0999ef83064cbb98b
Gabriel Moré Maia <gabriel_more@hotmail.com>
2020-10-01 10:43:38 -0300

    [teste] - deploy

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 
-require('./src/controllers/index')(app)
-require('./src/middlewares/error')(app)
 
-app.listen(8080, () => { Configuration.ENVIRONMENT === "stage" ? console.log("Server is running!!!") : null })
+
+app.listen(8080, () => { })
