0e04e5855d6c3b3729a8d7d5b89ec9da459cfcc5
Gabriel Moré Maia <gabriel_more@hotmail.com>
2020-10-01 10:47:51 -0300

    [test] - deploy

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => {
+  res.json({init : 'olá mundo'})
+})
 
-
-app.listen(8080, () => { })
+app.listen(8080, () => { console.log("server on") })
